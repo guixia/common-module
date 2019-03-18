e12ed72f753556b6635c87222278500c39702990
caoliang <caoliang@fenlibao.com>
2019-03-18 15:28:12 +0800

    修改基类

--- src/main/java/com/fenlibao/common/base/BaseActivity.java
+++ src/main/java/com/fenlibao/common/base/BaseActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.alibaba.android.arouter.launcher.ARouter;
 
 /**
- * 0318 1430
+ * 0318 1527
  */
 public abstract class BaseActivity extends AppCompatActivity {
     protected BaseActivity _activity;
